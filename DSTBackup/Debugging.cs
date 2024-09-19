801af265bac986492eb0725df816e610e478ad8e
Jakooob14 <sokoljakub14@gmail.com>
2024-09-19 22:46:17 +0200

    Added logging

---
+++ DSTBackup/Debugging.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace DSTBackup;
+
+public static class Debugging
+{
+    private static bool _initialized = false;
+    private static string _file = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss") + ".log";
+
+    public enum LoggingType
+    {
+        Info,
+        Warning,
+        Error
+    }
+    
+    public static void Initialize()
+    {
+        if (_initialized) return;
+        _initialized = true;
+
+        File.Create(_file);
+        Log("Started logging");
+    }
+    
+    public static void Log(string message, LoggingType loggingType = LoggingType.Info)
+    {
+        string prefix = $"[{DateTime.Now}] [{loggingType.ToString().ToUpper()}] ";
+        
+        Debug.WriteLine(prefix + message);
+        File.AppendAllText(_file, prefix + message + "\n");
+    }
+}
--- DSTBackup/MainWindow.xaml.cs
+++ DSTBackup/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using Microsoft.Win32;
 using Newtonsoft.Json;
+using Path = System.IO.Path;
 using Timer = System.Timers.Timer;
 
 namespace DSTBackup;
@@ -38,7 +39,8 @@ public partial class MainWindow : Window
         }
     }
 
-    readonly int _checkTime = 1;
+    private readonly int _checkTime = 1;
+    private readonly string _id = "DST Backup";
     
     List<World?> _worlds = new List<World?>();
     World? _worldToBackup = null;
@@ -52,35 +54,40 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        Debugger.Launch();
-        
-
-        config.Read();
-        PathInput.Text = config.BackupPath;
-        MaxWorldsInput.Text = config.MaxWorlds.ToString();
-        
-        string worldPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Documents\Klei\DoNotStarveTogether");
-        Debug.WriteLine($"[{DateTime.Now}] Initialized | World Path: {worldPath}");
-
-        string[] idFolders = Directory.GetDirectories(worldPath);
-
-        foreach (string idFolderPath in idFolders)
+        try
         {
-            if (idFolderPath.Contains("backup")) continue;
-            Debug.WriteLine($"[{DateTime.Now}] Foreach id | {idFolderPath}");
-            
-            string[] worldFolders = Directory.GetDirectories(idFolderPath + "\\CloudSaves");
-            foreach (string worldFolderPath in worldFolders)
+            config.Read();
+            PathInput.Text = config.BackupPath;
+            MaxWorldsInput.Text = config.MaxWorlds.ToString();
+        
+            string worldPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Documents\Klei\DoNotStarveTogether");
+            Debugging.Log($"Initialized | World Path: {worldPath}");
+
+            string[] idFolders = Directory.GetDirectories(worldPath);
+
+            foreach (string idFolderPath in idFolders)
             {
-                Debug.WriteLine($"[{DateTime.Now}] Foreach world | {worldFolderPath}");
-                INIFile iniFile = new INIFile(worldFolderPath + @"\cluster.ini");
-                string world = iniFile.IniReadValue("NETWORK", "cluster_name");
-                if (world == "") continue;
-                world = Regex.Replace(world, "[^a-zA-Z0-9 ]", "");
-                WorldInput.Items.Add(world);
-                _worlds.Add(new World(world, worldFolderPath));
-                Debug.WriteLine($"[{DateTime.Now}] Added world {world} in {worldFolderPath}");
+                if (idFolderPath.Contains("backup")) continue;
+                Debugging.Log($"Foreach id | {idFolderPath}");
+            
+                string[] worldFolders = Directory.GetDirectories(idFolderPath + "\\CloudSaves");
+                foreach (string worldFolderPath in worldFolders)
+                {
+                    Debugging.Log($"Foreach world | {worldFolderPath}");
+                    INIFile iniFile = new INIFile(worldFolderPath + @"\cluster.ini");
+                    string world = iniFile.IniReadValue("NETWORK", "cluster_name");
+                    if (world == "") continue;
+                    world = Regex.Replace(world, "[^a-zA-Z0-9 ]", "_");
+                    WorldInput.Items.Add(world);
+                    _worlds.Add(new World(world, worldFolderPath));
+                    Debugging.Log($"Added world {world} in {worldFolderPath}");
+                }
             }
+        }
+        catch (Exception exception)
+        {
+            Debugging.Log("Error at Initialize: " + exception, Debugging.LoggingType.Error);
+            MessageBox.Show("Error triggered! Look in the log file for more info.\n" + exception, _id, MessageBoxButton.OK, MessageBoxImage.Error);
         }
         
         timer.Interval = TimeSpan.FromSeconds(_checkTime);
@@ -102,107 +109,133 @@ public partial class MainWindow : Window
 
     private void StartBackupButton_Click(object sender, RoutedEventArgs e)
     {
-        switch (_backupStarted)
+        try
         {
-            case false:
+            switch (_backupStarted)
             {
-                try
+                case false:
                 {
-                    _worldToBackup = _worlds.First(item => item.Name == WorldInput.Text);
-                    Debug.WriteLine("Clicked Start Backup | _worldToBackup: " + _worldToBackup.Path);
+                    try
+                    {
+                        _worldToBackup = _worlds.First(item => item.Name == WorldInput.Text);
+                        Debugging.Log("Clicked Start Backup | _worldToBackup: " + _worldToBackup.Path);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception);
+                        return;
+                    }
+
+                    if (_worldToBackup == null) return;
+
+                    _backupStarted = true;
+                    StartBackupButton.Content = "Stop Backup";
+                    WorldInput.IsEnabled = false;
+                    PathInput.IsEnabled = false;
+                    BrowseButton.IsEnabled = false;
+                    MaxWorldsInput.IsEnabled = false;
+
+
+                    config.BackupPath = PathInput.Text;
+                    config.MaxWorlds = Convert.ToInt32(MaxWorldsInput.Text);
+                    Config.Write(config);
+
+                    Debugging.Log($"Timer started | Backup Path: {config.BackupPath} | MaxWorlds: {config.MaxWorlds}");
+                    timer.Start();
+                    break;
                 }
-                catch (Exception exception)
+                case true:
                 {
-                    Console.WriteLine(exception);
-                    return;
+                    _backupStarted = false;
+                    StartBackupButton.Content = "Start Backup";
+                    WorldInput.IsEnabled = true;
+                    PathInput.IsEnabled = true;
+                    BrowseButton.IsEnabled = true;
+                    MaxWorldsInput.IsEnabled = true;
+
+                    _oldLastModified = DateTime.MinValue;
+
+                    Debugging.Log("Clicked Stop Backup and stopped Timer");
+                    timer.Stop();
+                    break;
                 }
-        
-                if (_worldToBackup == null) return;
-                
-                _backupStarted = true;
-                StartBackupButton.Content = "Stop Backup";
-                WorldInput.IsEnabled = false;
-                PathInput.IsEnabled = false;
-                BrowseButton.IsEnabled = false;
-                MaxWorldsInput.IsEnabled = false;
-                
-
-                config.BackupPath = PathInput.Text;
-                config.MaxWorlds = Convert.ToInt32(MaxWorldsInput.Text);
-                Config.Write(config);
-                
-                Debug.WriteLine($"[{DateTime.Now}] Timer started | Backup Path: {config.BackupPath} | MaxWorlds: {config.MaxWorlds}");
-                timer.Start();
-                break;
             }
-            case true:
-            {
-                _backupStarted = false;
-                StartBackupButton.Content = "Start Backup";
-                WorldInput.IsEnabled = true;
-                PathInput.IsEnabled = true;
-                BrowseButton.IsEnabled = true;
-                MaxWorldsInput.IsEnabled = true;
-
-                _oldLastModified = DateTime.MinValue;
-                
-                Debug.WriteLine("Clicked Stop Backup and stopped Timer");
-                timer.Stop();
-                break;
-            }
+        }
+        catch (Exception exception)
+        {
+            Debugging.Log("Error at StartBackupButton_Click: " + exception, Debugging.LoggingType.Error);
+            MessageBox.Show("Error triggered! Look in the log file for more info.\n" + exception, _id, MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 
     private void timer_Tick(object? sender, EventArgs e)
     {
-        Debug.WriteLine($"[{DateTime.Now}] Start tick | _worldToBackup: {_worldToBackup.Path}");
-        if (_worldToBackup == null) return;
-        FileInfo masterInfo = new FileInfo(_worldToBackup.Path + @"\Master.zip");
-        FileInfo cavesInfo = new FileInfo(_worldToBackup.Path + @"\Caves.zip");
-
-        if (masterInfo.LastWriteTime > _oldLastModified)
+        try
         {
-            _oldLastModified = masterInfo.LastWriteTime;
-            Debug.WriteLine($"[{DateTime.Now}] Master older");
-        } else if (cavesInfo.LastWriteTime > _oldLastModified)
-        {
-            _oldLastModified = cavesInfo.LastWriteTime;
-            Debug.WriteLine($"[{DateTime.Now}] Caves older");
-        }
-        else
-        {
-            Debug.WriteLine($"[{DateTime.Now}] Else triggered");
-            return;
-        }
+            Debugging.Log($"Start tick | _worldToBackup: {_worldToBackup.Path}");
+            if (_worldToBackup == null) return;
+            FileInfo masterInfo = new FileInfo(_worldToBackup.Path + @"\Master.zip");
+            FileInfo cavesInfo = new FileInfo(_worldToBackup.Path + @"\Caves.zip");
 
-        
-        string idPattern = @"DoNotStarveTogether\\(\d+)\\CloudSaves";
-        string worldPattern = @"CloudSaves\\(.+)";
-        
-        Match idMatch = Regex.Match(_worldToBackup.Path, idPattern);
-        Match worldMatch = Regex.Match(_worldToBackup.Path, worldPattern);
-        Debug.WriteLine($"[{DateTime.Now}] Regex start");
-
-        if (worldMatch.Success && idMatch.Success)
-        {
-            Debug.WriteLine($"[{DateTime.Now}] Regex match success");
-            string idResult = idMatch.Groups[1].Value;
-            string worldResult = worldMatch.Groups[1].Value;
-            
-            if(!Directory.Exists(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name)){
-                Directory.CreateDirectory(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name);
-                Debug.WriteLine($"[{DateTime.Now}] Created directory {config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name}");
-            }
-
-            int fileCount = Directory.EnumerateFiles(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name).Count();
-            Debug.WriteLine($"[{DateTime.Now}] ! " + config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name + "\\" + worldResult + " | File count " + fileCount);
-            if (fileCount >= config.MaxWorlds)
+            if (masterInfo.LastWriteTime > _oldLastModified)
             {
-                foreach (var fi in new DirectoryInfo(@config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name).GetFiles().OrderByDescending(x => x.LastWriteTime).Skip(config.MaxWorlds - 1))
-                    fi.Delete(); Debug.WriteLine($"[{DateTime.Now}] Deleted {config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name}");
+                _oldLastModified = masterInfo.LastWriteTime;
+                Debugging.Log($"Master older");
             }
-            ZipFile.CreateFromDirectory(_worldToBackup.Path, config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name + "\\" + worldResult + DateTime.Now.ToString(" yyyy-MM-dd hh_mm_ss") + ".zip");
-            Debug.WriteLine($"[{DateTime.Now}] Zipped folder");
+            else if (cavesInfo.LastWriteTime > _oldLastModified)
+            {
+                _oldLastModified = cavesInfo.LastWriteTime;
+                Debugging.Log($"Caves older");
+            }
+            else
+            {
+                Debugging.Log($"Else triggered");
+                return;
+            }
+
+
+            string idPattern = @"DoNotStarveTogether\\(\d+)\\CloudSaves";
+            string worldPattern = @"CloudSaves\\(.+)";
+
+            Match idMatch = Regex.Match(_worldToBackup.Path, idPattern);
+            Match worldMatch = Regex.Match(_worldToBackup.Path, worldPattern);
+            Debugging.Log($"Regex start");
+
+            if (worldMatch.Success && idMatch.Success)
+            {
+                Debugging.Log($"Regex match success");
+                string idResult = idMatch.Groups[1].Value;
+                string worldResult = worldMatch.Groups[1].Value;
+
+                if (!Directory.Exists(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name))
+                {
+                    Directory.CreateDirectory(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name);
+                    Debugging.Log(
+                        $"Created directory {config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name}");
+                }
+
+                int fileCount = Directory
+                    .EnumerateFiles(config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name).Count();
+                Debugging.Log($"! " + config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name + "\\" +
+                              worldResult + " | File count " + fileCount);
+                if (fileCount >= config.MaxWorlds)
+                {
+                    foreach (var fi in new DirectoryInfo(@config.BackupPath + "\\" + idResult + "\\" +
+                                                         _worldToBackup.Name).GetFiles()
+                                 .OrderByDescending(x => x.LastWriteTime).Skip(config.MaxWorlds - 1))
+                        fi.Delete();
+                    Debugging.Log($"Deleted {config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name}");
+                }
+
+                
+                ZipFile.CreateFromDirectory(_worldToBackup.Path, Path.Combine(config.BackupPath, idResult, _worldToBackup.Name, worldResult + DateTime.Now.ToString(" yyyy-MM-dd hh_mm_ss")) + ".zip");
+                Debugging.Log($"Zipped folder to {config.BackupPath + "\\" + idResult + "\\" + _worldToBackup.Name + "\\" + worldResult + DateTime.Now.ToString(" yyyy-MM-dd HH_mm_ss") + ".zip"}");
+            }
+        }
+        catch (Exception exception)
+        {
+            Debugging.Log("Error at timer_Tick: " + exception, Debugging.LoggingType.Error);
+            MessageBox.Show("Error triggered! Look in the log file for more info.\n" + exception, _id, MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 
