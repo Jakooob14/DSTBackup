07df5aa7fb889c55514dd625286e0d27055abdcc
Jakooob14 <sokoljakub14@gmail.com>
2024-09-19 23:25:00 +0200

    test clickonce update 2

--- DSTBackup/MainWindow.xaml.cs
+++ DSTBackup/MainWindow.xaml.cs
@@ -54,7 +54,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        MessageBox.Show("test");
+        MessageBox.Show("test2");
         try
         {
             config.Read();
